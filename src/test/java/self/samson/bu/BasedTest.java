dc68e5700f4089c43eb4321a82be5fc22d0248c1
Samson <xiangxing_bu@163.com>
2020-04-10 21:04:04 +0800

    将构建工具从gradle切换到maven，使用slf4j-api，避免直接使用log4j-api

---
+++ src/test/java/self/samson/bu/BasedTest.java
@@ -0,0 +1,5 @@
+package self.samson.bu;
+
+public class BasedTest {
+
+}
--- src/test/java/self/samson/bu/log/Log4jTest.java
+++ src/test/java/self/samson/bu/log/Log4jTest.java
@@ -1,21 +1,19 @@
 package self.samson.bu.log;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Log4jTest {
 
-    private static final Logger LOGGER = LogManager.getLogger(Log4jTest.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Log4jTest.class);
 
     @Test
     public void configTest() {
         LOGGER.trace("trace");
         LOGGER.debug("debug");
         LOGGER.info("info");
+        LOGGER.warn("warn");
         LOGGER.error("error");
-        LOGGER.fatal("fatal");
-
-
     }
 }
